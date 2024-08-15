fa7c7596b12e3837caf56a427046927c46a2d542
Cecilia Escoto <celiescotosandoval@gmail.com>
2024-08-15 15:37:26 -0600

    Se realiza el envio del primer inciso del primer ejericicio reescribir el ejemplo de arrays en ruby

---
+++ array.rb
@@ -0,0 +1,10 @@
+#array
+semana= ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
+puts semana
+
+puts "\nImprimir por posicion"
+
+puts semana[1]
+puts semana[3]
+puts semana[5]
+
