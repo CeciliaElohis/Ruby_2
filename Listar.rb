b16a81a25c89d99d10c303abecad9e8956d386bd
Cecilia Escoto <celiescotosandoval@gmail.com>
2024-08-16 14:56:32 -0600

    Se realiza el envio del segundo ejercicio,donde el se utiliza un Hash para buscar informacion que se solicita

---
+++ Listar.rb
@@ -0,0 +1,33 @@
+ListaHash= {}
+ListaHash['Maria'] = '2248-6559'
+ListaHash['Pedro'] = '9845-6532'
+ListaHash['Juan'] = '8265-4536'
+ListaHash['Alberto'] = '7896-4514'
+ 
+puts "Nombre".ljust(10)+"Celular"        
+  ListaHash.each do |nombre, celuar|
+  puts "#{nombre}".ljust(10)+"#{celuar}"
+  end
+
+puts "-------------------" 
+
+search = ""
+
+flag = false
+puts "Ingrese un nombre"
+search= gets.chomp
+search_cap = search.capitalize
+
+ListaHash.each do |nombre, celuar|
+  if search_cap == nombre
+    puts "-------------------" 
+    puts "Nombre".ljust(10)+"Celular" 
+    puts "#{nombre}".ljust(10)+"#{celuar}" 
+    flag = true
+    break
+  end
+end
+
+if flag == false
+  puts "No se encuentra ese nombre en la lista"
+end
